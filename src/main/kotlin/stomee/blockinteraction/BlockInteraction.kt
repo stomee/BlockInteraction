60a31e3489c5228522ef8111e3890e0d46ed7064
LeoDog896 <LeoDog896@hotmail.com>
2021-05-07 09:58:18 -0400

    Hook events

--- src/main/kotlin/stomee/blockinteraction/BlockInteraction.kt
+++ src/main/kotlin/stomee/blockinteraction/BlockInteraction.kt
@@ -1,14 +1,21 @@
 package stomee.blockinteraction
 
 import net.minestom.server.extensions.Extension;
+import world.cepi.kstom.Manager
 
 class BlockInteraction : Extension() {
 
     override fun initialize() {
+
+        Manager.connection.addPlayerInitialization(::hook)
+
         logger.info("[BlockInteraction] has been enabled!")
     }
 
     override fun terminate() {
+
+        Manager.connection.removePlayerInitialization(::hook)
+
         logger.info("[BlockInteraction] has been disabled!")
     }
 
